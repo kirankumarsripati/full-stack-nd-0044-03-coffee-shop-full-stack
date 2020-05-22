4d5ef266b8b985a0f9765e1b96d447b7439e6c47
Kirankumar Sripati <krnsripati@gmail.com>
2020-05-22 20:15:21 +0530

    feat: add auth0 in environment

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,7 +5,7 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-jv5b18wv.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'dev'
 
